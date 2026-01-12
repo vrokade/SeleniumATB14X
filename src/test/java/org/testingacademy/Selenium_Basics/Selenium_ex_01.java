a15f085fe68571199aff0f4bbc8eca66ddb71021
Vishal.Rokade <vrokade2013@gmail.com>
2026-01-12 21:50:55 +0530

    12/01

--- src/test/java/org/testingacademy/Selenium_Basics/Selenium_ex_01.java
+++ src/test/java/org/testingacademy/Selenium_Basics/Selenium_ex_01.java
@@ -1,9 +1,16 @@
 package org.testingacademy.Selenium_Basics;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class Selenium_ex_01 {
 
     @Test
     public void testBasics(){
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://google.com");
 
     }
 }
